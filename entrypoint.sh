2e6c9deb89010e4e031da66d7f58be6fef7f321b
TermuHACK <168407112+TermuHACK@users.noreply.github.com>
2025-06-12 13:46:19 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Запуск Shadowsocks
-ssserver --config config.json
+# Запуск shadowsocks-libev
+ss-server -s 0.0.0.0 -p 8388 -k mypassword -m aes-256-gcm --fast-open &
 
 # Запуск gotty на порту 7681
 gotty -w --port 7681 bash &
 
-# Поддержка фоновых процессов
+# Ожидание фоновых процессов
 wait
