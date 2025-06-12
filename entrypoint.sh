4e38c35d300108dfd64dab04a3990a3e444586c9
TermuHACK <168407112+TermuHACK@users.noreply.github.com>
2025-06-12 17:30:29 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,2 @@
-#!/bin/sh
-
-# Пример запуска shadowsocks и gotty
-ss-server -s 0.0.0.0 -p 8388 -k password -m aes-256-gcm &
-
-gotty --port 8080 bash
+xray -c /xray/config.json
+shadowsocks-libev -c /shadowsocks/config.json
