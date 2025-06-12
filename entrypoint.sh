62635048d9ed14c332e6296eac916f1d3a8d7c65
TermuHACK <168407112+TermuHACK@users.noreply.github.com>
2025-06-12 17:37:55 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 xray -c /xray/config.json
-ss-server -c config.json -p 8388 --plugin v2ray-plugin --plugin-opts "example.com"
+ss-server -c /shadowsocks/config.json -p 8388 --plugin v2ray-plugin --plugin-opts "example.com"
