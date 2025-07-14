d197e4a43e9acfca38e699738772e2429daa5bc7
TermuHACK <168407112+TermuHACK@users.noreply.github.com>
2025-07-14 21:32:01 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "🖥️ Запускаем gotty на 127.0.0.1:9000"
-/usr/local/bin/gotty --port 9000 --permit-write --title-format "🛠 Web Shell" bash &
+/usr/local/bin/gotty -p 9000 bash &
 
 echo "🌐 Запускаем nginx на 0.0.0.0:80"
 nginx -g "daemon off;"
