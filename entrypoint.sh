ba4bdf2eb14c78beda5bf09d5b22351459a60c24
TermuHACK <168407112+TermuHACK@users.noreply.github.com>
2025-07-19 15:17:38 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 apk update
-apk add tmux tmate
-# Показать неофетч
-#neofetch || true
-tmate
+
 
 # Запуск nginx
 nginx
 
 # Ожидание запуска
 sleep 2
-
-ttyd --writable -p 8080 disableReconnect=true -t fontSize=16 bash
-
-tinyproxy
+tinyproxy -d -p 8080 -a 127.0.0.1
 # Чтобы контейнер не завершился
 tail -f /dev/null
